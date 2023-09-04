1a467bdddfdc6d5157d5f5d60e7fce03d799e884
Jaciel Sanchez <jacielsanchez1719@gmail.com>
2023-09-03 20:55:51 -0700

    Respaldo
    Respaldo formateo

---
+++ IA/Red convolucional/EntrenamientoModelo.py
@@ -0,0 +1,30 @@
+import cv2
+import os
+import numpy as np
+
+dataPath = 'C:/Users/XxJacSan17xX/Desktop/RedConvolusional/Data'
+peopleList = os.listdir(dataPath)
+print('Lista de personas: ', peopleList)
+labels = []
+facesData = []
+label = 0
+for nameDir in peopleList:
+    personPath = dataPath + '/' + nameDir
+    print('Leyendo las imágenes')
+    for fileName in os.listdir(personPath):
+        print('Rostros: ', nameDir + '/' + fileName)
+        labels.append(label)
+        facesData.append(cv2.imread(personPath+'/'+fileName,0))
+
+    label = label + 1
+
+
+# Métodos para entrenar el reconocedor
+face_recognizer = cv2.face.EigenFaceRecognizer_create()
+
+# Entrenar el reconocedor
+print("Entrenando...")
+face_recognizer.train(facesData, np.array(labels))
+
+face_recognizer.write('modelEigenFaceRecognizer.xml')
+print("Modelo almacenado...")
